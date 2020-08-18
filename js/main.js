7aa4667acbba479b5353980bc119e90a2bfd3a17
davidabutler92 <davidabutler92@yahoo.com>
2020-08-18 16:25:44 -0700

    add on submit show work-responses, forEach checked box, grab value, append them to work-responses, then hide the survey

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $("#transportaion-survey").submit(function(event) {
     event.preventDefault();
+    $("#work-responses").show();
+    $("input: checkbox[name=work-transportation]:checked").each(function() {
+      const workTransportMode = $(this).val();
+      $("#work-responses"),append(workTransportMode + "<br>");
+    });
     
   });
 });
