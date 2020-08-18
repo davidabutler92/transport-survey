b358c5a471681cb5bea03d049c57a62b9efec29e
davidabutler92 <davidabutler92@yahoo.com>
2020-08-18 16:32:01 -0700

    add debug, typo error

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $("#transportaion-survey").submit(function(event) {
+  $("form#transportation-survey").submit(function(event) {
     event.preventDefault();
     $("#work-responses").show();
-    $("input: checkbox[name=work-transportation]:checked").each(function() {
-      const workTransportMode = $(this).val();
-      $("#work-responses"),append(workTransportMode + "<br>");
+    $("input:checkbox[name=work-transportation]:checked").each(function() {
+      const workTransportationMode = $(this).val();
+      $('#work-responses').append(workTransportationMode + "<br>");
     });
-    
+    $('#transportation_survey').hide();
   });
 });
